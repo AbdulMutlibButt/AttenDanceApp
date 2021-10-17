b077cdc402d3098a635474bcb8f55b8ecfb15ddb
AbdulMutlibB <mutlibbutt209@gmail.com>
2021-10-17 16:37:08 +0500

    Attendance App UI complete

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React from "react";
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./Components/Login";
-import Home from './Components/Home'
+import Home from "./Components/Home";
 import MenuAppBar from "./Components/Header";
 import SideBar from "./Components/SideBar";
+import Students from "./Components/Students";
+import CLasses from "./Components/Classes";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Switch>
-          <Route path="/" exact >
+          <Route path="/" exact>
             <Login />
           </Route>
           <Route path="/home">
-            <MenuAppBar/>
-            <Home/> 
-            <SideBar/>
+            <MenuAppBar />
+            <Home />
+            <SideBar />
           </Route>
-          <Route path="/user">
-
+          <Route path="/Student">
+            <MenuAppBar />
+            <Students />
+            <SideBar />
+          </Route>
+          <Route path="/Teacher">
+            <MenuAppBar />
+            <Home />
+            <SideBar />
+          </Route>{" "}
+          <Route path="/Class">
+            <MenuAppBar />
+            <CLasses />
+            <SideBar />
           </Route>
         </Switch>
       </Router>
